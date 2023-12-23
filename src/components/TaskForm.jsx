c508e644f99c7b94f4709a5e14606069ddb824f1
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-23 09:30:00 +0530

    ui: add completed task styling

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -19,6 +19,13 @@ export default function TaskForm({ addTask }) {
                 placeholder="Enter new task"
                 onKeyDown={(e) => e.key === "Enter" && submit()}
             />
+            <span
+                className={task.completed ? "text-decoration-line-through text-muted" : ""}
+                style={{ cursor: "pointer" }}
+                onClick={() => toggleTask(task.id)}
+            >
+                {task.text}
+            </span>
 
 
             <button className="btn btn-primary mt-2" onClick={submit}>
