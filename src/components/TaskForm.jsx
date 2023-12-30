9963e6acffd5ff9d516a3483349e568b68897354
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-30 10:30:00 +0530

    feat: clean up TaskForm and add dark mode toggle

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,69 +2,49 @@ import { useState } from "react";
 
 export default function TaskForm({ addTask }) {
     const [text, setText] = useState("");
+    const [darkMode, setDarkMode] = useState(false);
 
     const submit = () => {
         if (!text.trim()) return;
+        if (text.trim().length < 3) {
+            alert("Task must be at least 3 characters");
+            return;
+        }
 
         addTask(text);
         setText("");
     };
 
     return (
-        <div className="mb-3">
+        <div
+            className={`p-4 rounded ${darkMode ? "bg-dark text-light" : "bg-light"
+                }`}
+        >
+            {/* Minor UI tweak for daily progress */}
+
+            <div className="d-flex justify-content-between align-items-center mb-3">
+                <h5 className="m-0">Add New Task</h5>
+
+                <button
+                    className="btn btn-sm btn-secondary"
+                    onClick={() => setDarkMode(!darkMode)}
+                >
+                    {darkMode ? "Light Mode" : "Dark Mode"}
+                </button>
+            </div>
+
             <input
-                className="form-control"
+                className="form-control mb-2"
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 placeholder="Enter new task"
                 onKeyDown={(e) => e.key === "Enter" && submit()}
             />
-            <span
-                className={task.completed ? "text-decoration-line-through text-muted" : ""}
-                style={{ cursor: "pointer" }}
-                onClick={() => toggleTask(task.id)}
+
+            <button
+                className="btn btn-primary w-100"
+                onClick={submit}
             >
-                {task.text}
-                <const handleToggle= (id) => toggleTask(id);
-const handleDelete = (id) => deleteTask(id);
-const handleSave = (id) => {
-  if (editText.trim().length < 3) return alert("Text too short");
-                updateTask(id, editText);
-                setEditingId(null);
-                setEditText("");
-};
-
-
-const completeAll = () => {
-                    setTasks(tasks.map(t => ({ ...t, completed: true })));
-};
-                <button
-                    className="btn btn-outline-success me-2"
-                    onClick={completeAll}
-                >
-                    Complete All
-                </button>
-const deleteCompleted = () => {
-                    setTasks(tasks.filter(t => !t.completed));
-};
-                <button
-                    className="btn btn-outline-danger"
-                    onClick={deleteCompleted}
-                >
-                    Delete Completed
-                </button>
-                const [darkMode, setDarkMode] = useState(false);
-                <div className={`container ${darkMode ? "bg-dark text-light" : ""}`}>
-                    <button
-                        className="btn btn-sm btn-secondary mb-3"
-                        onClick={() => setDarkMode(!darkMode)}
-                    >
-                        {darkMode ? "Light Mode" : "Dark Mode"}
-                    </button>
-                    <div className={`container ${darkMode ? "bg-dark text-light" : ""} p-4 rounded`}>
-                        // Minor UI tweak for daily progress
-
-            <button className="btn btn-primary mt-2" onClick={submit}>
                 Add Task
             </button>
         </div>
